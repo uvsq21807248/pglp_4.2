cd7484f8f6d505f1b979b5c0d5da31cd71708115
uvsq21807248 <atmanihajar09@gmail.com>
2020-03-23 18:45:35 +0100

    creation de la command quit

---
+++ src/main/java/exo4/exo4_2/Quit.java
@@ -0,0 +1,13 @@
+package exo4.exo4_2;
+
+public class Quit implements GenericCom {
+	public void apply() {
+		System.out.println("Programme quité");
+	}
+
+	public void appl() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
