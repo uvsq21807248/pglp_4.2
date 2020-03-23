946be8c08863cdc2e00dec8ebafb9aeb8e955950
uvsq21807248 <atmanihajar09@gmail.com>
2020-03-23 21:18:12 +0100

    creation de la command undo

---
+++ src/main/java/exo4/exo4_2/Undo.java
@@ -0,0 +1,19 @@
+package exo4.exo4_2;
+import java.util.Stack;
+public class Undo implements GenericCom{
+	private Stack<Stack<Double>> undo;
+	private Stack<Double> pile;
+	public void apply() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void appl() {
+		// TODO Auto-generated method stub
+		
+	}
+	public Undo(Stack<Double> pile) {
+		super();
+		this.pile = pile;
+	}
+	
+}
