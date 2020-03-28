73c463c32ae5dae77ec0e761c081878348e32201
uvsq21807248 <atmanihajar09@gmail.com>
2020-03-28 15:12:45 +0100

    ajout de CalculatriceRPN

---
+++ src/main/java/exo4/exo4_2/CalculatriceRPN.java
@@ -0,0 +1,11 @@
+package exo4.exo4_2;
+
+public enum CalculatriceRPN {
+	
+	CALCULATRICE;
+
+    public static void main(final String[] args) throws Exception {
+        SaisieRPN saisie = new SaisieRPN();
+        saisie.calcul();
+    }
+}
